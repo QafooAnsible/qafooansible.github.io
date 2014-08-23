4801eba6935d80e2baeb9630f49d9882481e9a12
Jakob Westhoff <jakob@westhoffswelt.de>
2014-08-23 16:50:35 +0200

    Implemented simple github api based satis config updater

---
+++ Application Support/updateSatisJson.php
@@ -0,0 +1,59 @@
+<?php
+$blacklist = array(
+    "QafooAnsible/qafooansible.github.io" => true
+);
+
+$githubUrl = "https://api.github.com/orgs/QafooAnsible/repos";
+
+$responseBody = file_get_contents(
+    $githubUrl,
+    false,
+    stream_context_create(
+        array(
+            "http" => array(
+                "method" => "GET",
+                "header" => array(
+                    "Accept: application/vnd.github.v3+json",
+                    "User-Agent: QafooAnsible Satis Updater"
+                )
+            )
+        )
+    )
+);
+
+echo "Fetching repository list.\n";
+
+if ($responseBody === false) {
+    throw new \RuntimeException("Could not fetch repository list from github");
+}
+
+$repositories = json_decode($responseBody, true);
+
+if ($repositories === null) {
+    throw new \RuntimeException("Retrieved JSON could not be decoded");
+}
+
+$repositories = array_filter($repositories, function($repository) use ($blacklist) {
+    return array_key_exists($repository["full_name"], $blacklist) === false;
+});
+
+$satisRepositories = array_map(function($repository) {
+    return array(
+        "type" => "vcs",
+        "url" => $repository["html_url"]
+    );
+}, $repositories);
+
+
+$satisConfiguration = array(
+    "name" => "QafooAnsible",
+    "homepage" => "http://qafooansible.github.io",
+    "repositories" => $satisRepositories
+);
+
+file_put_contents(
+    "satis.json",
+    json_encode($satisConfiguration, JSON_PRETTY_PRINT)
+);
+
+echo "satis.json updated.\n";
